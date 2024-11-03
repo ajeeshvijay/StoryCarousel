f6a71b02a9c89671dc2377993f7d888cb185e04e
ajeeshvijay <ajeeshvijay007@gmail.com>
2024-11-03 12:59:52 +0530

    Initial commit

---
+++ story.js
@@ -0,0 +1,12 @@
+class Story {
+
+    constructor(element, opts) {
+        this.element = element;
+        this.opts = opts;
+        this.init()
+    }
+
+    init() {
+        console.log(`Options: ${this.opts}`);
+    }
+}
