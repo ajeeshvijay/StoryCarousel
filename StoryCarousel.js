a7b290387820c80ec7f5acfa6f0f26798a80a9b3
ajeeshvijay <ajeeshvijay007@gmail.com>
2024-11-23 01:37:26 +0530

    indicator animation improvements

--- StoryCarousel.js
+++ StoryCarousel.js
@@ -3,6 +3,8 @@ class StoryCarousel {
   opt = {
     duration: 3000
   };
+  startAnimate = null;
+  startAnimateInterval = null;
 
   constructor(element, opts) {
     this.element = element;
@@ -17,15 +19,16 @@ class StoryCarousel {
 
     this._wrap = document.createElement('div');
     this._wrap.classList.add('story-carousel-wrap');
-  
+
     this._wrap.appendChild(this.element.querySelector('.story-carousel-items'));
     this.element.appendChild(this._wrap);
 
     this.buildIndicators();
     this.animate();
+    this.navigation();
   }
 
-  buildIndicators() { 
+  buildIndicators() {
     const indicators = document.createElement('div');
     indicators.classList.add('story-carousel-indicators');
     this._items.forEach((item, index) => {
@@ -33,8 +36,8 @@ class StoryCarousel {
       indicator.classList.add('story-carousel-indicator');
       const loading = document.createElement('i');
       loading.classList.add('story-carousel-indicator-loading');
-      // loading.style.transitionDuration = this.opts.duration + 'ms';
-      loading.setAttribute('data-duration', this.opts.duration + 'ms');
+      loading.style.transitionDuration = this.opts.duration + 'ms';
+      // loading.setAttribute('data-duration', this.opts.duration + 'ms');
       indicator.appendChild(loading);
       indicators.appendChild(indicator);
     });
@@ -43,20 +46,27 @@ class StoryCarousel {
   }
 
   animate() {
-    this._items[this._index].classList.add('story-carousel-item-active');
-    this._indicators[this._index].classList.add('story-carousel-indicator-active');
-    this._index++;
-    setInterval(() => {
-      this._items.forEach((item, i) => {
-        item.classList.remove('story-carousel-item-active')
-        this._indicators[i].classList.remove('story-carousel-indicator-active')
-      });
+    clearTimeout(this.startAnimate);
+    clearInterval(this.startAnimateInterval);
+    this.startAnimate = setTimeout(() => {
       this._items[this._index].classList.add('story-carousel-item-active');
-      this._indicators[this._index].classList.add('story-carousel-indicator-active')
+      this._indicators[this._index].classList.add('story-carousel-indicator-active');
+
       this._index++;
-      if (this._index >= this._items.length) {
-        this._index = 0;
-      }
-    }, this.opts.duration);
+      this.startAnimateInterval = setInterval(() => {
+        this._items.forEach((item, i) => {
+          item.classList.remove('story-carousel-item-active');
+          this._indicators[i].classList.remove('story-carousel-indicator-active');
+          this._indicators[i].querySelector('.story-carousel-indicator-loading').style.transitionDuration = '0ms';
+        });
+        this._items[this._index].classList.add('story-carousel-item-active');
+        this._indicators[this._index].classList.add('story-carousel-indicator-active');
+        this._indicators[this._index].querySelector('.story-carousel-indicator-loading').style.transitionDuration = this.opts.duration + 'ms';
+        this._index++;
+        if (this._index >= this._items.length) {
+          this._index = 0;
+        }
+      }, this.opts.duration);
+    }, 0);
   }
 }
